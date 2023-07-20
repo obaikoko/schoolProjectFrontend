56bb69542a9a4d312be032d6f3383cfb5c865d5e
Okechukwu Chidera <okechukwuchidera12@gmail.com>
2023-07-20 07:00:09 +0100

    Added styles to the news secction

--- components/NewsSection.jsx
+++ components/NewsSection.jsx
@@ -1,42 +1,39 @@
-import React from 'react';
+import React from "react";
+import style from "../styles/schoolHighlights.module.css";
 
 const NewsSection = () => {
   // Assuming you have an array of news articles
   const newsArticles = [
     {
-      title: 'School Carnival',
-      date: 'June 15, 2023',
+      title: "School Carnival",
+      date: "June 15, 2023",
       content:
-        'Join us for our annual school carnival on June 15th. There will be games, food, and lots of fun activities for everyone!',
+        "Join us for our annual school carnival on June 15th. There will be games, food, and lots of fun activities for everyone!",
     },
     {
-      title: 'Science Fair Winners',
-      date: 'June 20, 2023',
+      title: "Science Fair Winners",
+      date: "June 20, 2023",
       content:
-        'Congratulations to our talented students who won the science fair competition. Their innovative projects impressed the judges!',
+        "Congratulations to our talented students who won the science fair competition. Their innovative projects impressed the judges!",
     },
     // Add more news articles as needed
   ];
 
   return (
-    <div className='container'>
-      <h2>News Section</h2>
-      <div className='row'>
-        {newsArticles.map((article, index) => (
-          <div key={index} className='col-md-6 col-lg-4 mb-4'>
-            <div className='card'>
-              <div className='card-body'>
-                <h5 className='card-title'>{article.title}</h5>
-                <h6 className='card-subtitle mb-2 text-muted'>
-                  {article.date}
-                </h6>
-                <p className='card-text'>{article.content}</p>
-              </div>
+    <section className={style.highlight}>
+      <div className={style.container}>
+        <h2 className={style.heading}>News Section</h2>
+        <div className={style.card}>
+          {newsArticles.map((article, index) => (
+            <div key={index} className={style.card_body}>
+              <h2 className={style.card_title}>{article.title}</h2>
+              <h6 className={style.card_date}>{article.date}</h6>
+              <p className={style.card_text}>{article.content}</p>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
